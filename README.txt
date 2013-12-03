cb45a00edfe8879fe3b2d29ea294228c9bad6d11
MarkAWard <markmaw@gmail.com>
2013-12-03 00:06:22 -0500

    Crawler object added

--- README.txt
+++ README.txt
@@ -1,11 +1,11 @@
 Web Crawler written in Python
 
-At this point the crawler can return an index of all words that were found. And with each word a list of urls where that word can be found.
+At this point the crawler can return an index of all words that were found along with a graph of the pages found. And with each word a list of urls where that word can be found.
 
 TODO
 Improve the list of words put into the index. 
 Remove words such as: a, the, it, is...
 Only get words from certain sections of the source
-Implement PageRank
+Put beautiful soup in there
 
-Not a very polite crawler yet. Do not run
+
--- crawling.py
+++ crawling.py
@@ -1,82 +1,80 @@
 import urllib2
 
-# list_a = list_a U list_b
-def union(list_a, list_b):
-    for item in list_b:
-        if item not in list_a:
-            list_a.append(item)
 
-# return the next found url and end postion of the url
-# to then be able to continue searching
-def find_next_link(page_source):
-    # could be improved... reg exp?
-    link_tag = page_source.find('<a href=')
-    if link_tag < 0:
-        return None, 0
+class Crawler(object):
 
-    # add 1 to start at the first char after "    
-    beg_url = page_source.find('"', link_tag) + 1
-    end_url = page_source.find('"', beg_url)
-    url = page_source[beg_url:end_url]
-
-    return url, end_url
-
-# continuously call find_next_link till it returns
-# none. At each step add the found link to the list
-def get_all_links(page_source):
-    links = []
-    while True:
-        url, next_index = find_next_link(page_source)
-        if not url:
-            break
-        links.append(url)
-        page_source = page_source[next_index:]
-    return links
-
-# Crawl a webpage passed in as seed. Every link found in the seed's
-# html source code is added to the list tocrawl. Depth first search.
-# Each page that is crawled is added to the index dictionary. When 
-# there are no more pages to be crawled, return the index
-def crawl(seed):
-    tocrawl = [seed]
-    crawled = []
     index = {}
     graph = {}
-    while tocrawl:
-        # next page to crawl
-        page = tocrawl.pop()
-        if page not in crawled:
-            page_source_code = fetch(page)
-            add_page_to_index(index, page, page_source_code)
-            outlinks = get_all_links(page_source_code)
-            graph[page] = outlinks
-            # add newly found links to the tocrawl list
-            union(tocrawl, outlinks)
-            crawled.append(page)
-    return index
+    seed = ""
+
+    def __init__(self):
+        self.index = {}
+        self.graph = {}
+
+    # Crawl a webpage passed in as seed. Every link found in the seed's
+    # html source code is added to the list tocrawl. Depth first search.
+    # Each page that is crawled is added to the index dictionary. When 
+    # there are no more pages to be crawled, return the index
+    def crawl(self, web_seed):
+        self.seed = web_seed
+        tocrawl = [web_seed]
+        crawled = []
+        while tocrawl:
+            # next page to crawl
+            page = tocrawl.pop()
+            if page not in crawled:
+                page_source_code = self.fetch(page)
+                self.add_page_to_index(self.index, page, page_source_code)
+                outlinks = self.get_all_links(page_source_code)
+                self.graph[page] = outlinks
+                # add newly found links to the tocrawl list
+                self.union(tocrawl, outlinks)
+                crawled.append(page)
+            print tocrawl
+        return self.index, self.graph
 
 
-def fetch(url):
-    try:
-        request = urllib2.urlopen(url)
-        return request.read(0)
-    except:
-        return ""
+    def fetch(self, url):
+        try:
+            request = urllib2.urlopen(url)
+            return request.read()
+        except:
+            return ""
 
 
 
-def lookup(index,keyword):
-    if keyword in index:
-        return index[keyword]
-    else:
-        return None
+    # return the next found url and end postion of the url
+    # to then be able to continue searching
+    def find_next_link(self, page_source):
+        # could be improved... reg exp?
+        link_tag = page_source.find('<a href=')
+        if link_tag < 0:
+            return None, 0
+    # add 1 to start at the first char after "    
+        beg_url = page_source.find('"', link_tag) + 1
+        end_url = page_source.find('"', beg_url)
+        url = page_source[beg_url:end_url]
+        
+        return url, end_url
 
-# add keyword url pair to the index dictionary
-def add_to_index(index,keyword,url):
-    if keyword in index:
-        index[keyword].append(url)
-    else:
-        index[keyword] = [url]
+    # continuously call find_next_link till it returns
+    # none. At each step add the found link to the list
+    def get_all_links(self, page_source):
+        links = []
+        while True:
+            url, next_index = self.find_next_link(page_source)
+            if not url:
+                break
+            links.append(url)
+            page_source = page_source[next_index:]
+            return links
+
+    # add keyword url pair to the index dictionary
+    def add_to_index(self, index, keyword, url):
+        if keyword in index:
+            index[keyword].append(url)
+        else:
+            index[keyword] = [url]
 
 
 
@@ -89,27 +87,36 @@ def add_to_index(index,keyword,url):
 # content is url's source code, split it into words and
 # add each word to the index with the url.
 # Dont like it right now....
-def add_page_to_index(index,url,content):
-    # define characters to split at and strip
-    splitlist = ' ,.-<>!#/\\'
-    for word in split_string(content,splitlist):
-        if not lookup(index,word):
-            add_to_index(index,word,url)
+    def add_page_to_index(self, index, url, content):
+        # define characters to split at and strip
+        splitlist = ' ,.-<>!#/\\'
+        for word in self.split_string(content,splitlist):
+            if not lookup(index,word):
+                self.add_to_index(index,word,url)
             
 ################# FIX ME #################################
-def split_string(source,splitlist):
-    words = []
-    atSplit = True
-    for char in source:
-        if char in splitlist:
-            atSplit = True
-        else:
-            if atSplit:
-                words.append(char)
-                atSplit = False
+    def split_string(self, source, splitlist):
+        words = []
+        atSplit = True
+        for char in source:
+            if char in splitlist:
+                atSplit = True
             else:
-                words[-1] = words[-1] + char
-    return words
+                if atSplit:
+                    words.append(char)
+                    atSplit = False
+                else:
+                    words[-1] = words[-1] + char
+        return words
+
+# list_a = list_a U list_b
+    def union(self, list_a, list_b):
+        try:
+            for item in list_b:
+                if item not in list_a:
+                    list_a.append(item)
+        except:
+            return
 
 
 # version of pagerank algorithm
@@ -134,3 +141,19 @@ def compute_ranks(graph):
             newranks[page] = newrank
         ranks = newranks
     return ranks
+
+
+def lookup(index,keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
+
+if __name__ == "__main__":
+    C = Crawler()
+    C.crawl("") # enter seed
+
+    print C.index
+    print "\n\n", C.graph
+    print "\n\n", C.seed
+   
